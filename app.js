0ebf4d812b52e1aa26820204c15151334e48bf18
Robin Verona <robin.anorev@proton.me>
2022-09-25 11:11:14 +0200

    add hover reveal effect snippet

---
+++ app.js
@@ -0,0 +1,24 @@
+
+let links = [...document.querySelectorAll('.menu-item')];
+let linkImages = document.querySelectorAll('.hover-reveal__img');
+
+links.forEach((link, idx) => {
+  link.addEventListener('mousemove', (e) => {
+    link.children[1].style.opacity = 1;
+    link.children[0].style.zIndex = 3; 
+    link.children[1].style.transform = `translate(${e.clientX - 400}px, -${e.clientY / 3}px) rotate(${e.clientX / 50}deg)`;
+    linkImages[idx].style.transform = `scale(1, 1)`;
+    link.style.zIndex = 2; 
+
+  })
+
+  link.addEventListener('mouseleave', (e) => {
+    link.children[1].style.opacity = 0;
+    link.children[1].style.transform = `translate(${-e.clientX}px, -400px)`;
+    linkImages[idx].style.transform = `scale(0.8, 0.8)`;
+    link.style.zIndex = 0; 
+
+
+
+  });
+});
